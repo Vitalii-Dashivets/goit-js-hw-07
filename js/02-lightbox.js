ae12b301cccba97520c0801f1ab0595da8a679c2
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-05-08 12:56:40 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const itemsPreviewMarkup = galleryItems.map(item =>
+ `<li class="gallery__item">
+  <a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      alt=${item.description}
+    />
+  </a>
+</li>`).join('');
+
+
+   
+
+const galleryBox = document.querySelector('.gallery');
+galleryBox.insertAdjacentHTML('beforeend', itemsPreviewMarkup);
+
+
+var gallery = new SimpleLightbox('.gallery', { captionDelay: 250, captionsData: 'alt' });
+// const gallery = $('.gallery').simpleLightbox();
+
+gallery.on('show.simplelightbox', function (event) {
+    event.preventDefault();
+    console.log("click");
+  gallery.open();
+  });
+
+ 
+// console.log(gallery);
